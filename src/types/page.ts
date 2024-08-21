0f5807df8a4ceb451d89dc8df312fe3d48b4673a
Stephen Ferguson <steve.ferguson@uscca.com>
2024-08-21 17:37:23 -0500

    prettier

--- src/types/page.ts
+++ src/types/page.ts
@@ -1,21 +1,21 @@
 export type HeroImage = {
-  id: string;
-  url: string;
-  alt: string;
-  mimeType: "video/mp4" | "image/jpeg" | "image/png";
-};
+  id: string
+  url: string
+  alt: string
+  mimeType: 'video/mp4' | 'image/jpeg' | 'image/png'
+}
 
 export type Page = {
-  title: string;
-  slug: string;
-  publishedAt: string;
-  updatedAt: string;
-  heroImage: HeroImage;
+  title: string
+  slug: string
+  publishedAt: string
+  updatedAt: string
+  heroImage: HeroImage
   body: {
-    text: string;
-  };
-};
+    text: string
+  }
+}
 
 export type Pages = {
-  pages: Page[];
-};
+  pages: Page[]
+}
